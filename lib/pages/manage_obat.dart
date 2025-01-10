715d22a139b9d764e49d58114a59e2ba2e6ca02a
Elsa <renetaelza@gmail.com>
2025-01-10 14:57:29 +0700

    Fixing manage obat

--- lib/pages/manage_obat.dart
+++ lib/pages/manage_obat.dart
@@ -42,28 +42,68 @@ class _ManageObatState extends State<ManageObat> {
   }
 
   void deleteObat(String id) {
-    FirebaseFirestore.instance.collection('obat').doc(id).delete();
+    FirebaseFirestore.instance
+        .collection('obat')
+        .doc(id)
+        .delete()
+        .then((value) {
+      fetchObat();
+    });
   }
 
-  void addObat(String nama, int harga, int stok) {
+  void addObat(String nama, int harga, int stok, String desc, String kategori,
+      String pict) {
     FirebaseFirestore.instance.collection('obat').add({
       'nama': nama,
       'harga': harga,
       'stok': stok,
+      'desc': desc,
+      'kategori': kategori,
+      'pict': pict,
     }).then((value) {
-      fetchObat();  // Refresh the list after adding
+      fetchObat();
     });
   }
 
-  // Update Obat function
-  void updateObat(String id, String nama, int harga, int stok) {
+  void updateObat(String id, String nama, int harga, int stok, String desc,
+      String kategori, String pict) {
     FirebaseFirestore.instance.collection('obat').doc(id).update({
       'nama': nama,
       'harga': harga,
       'stok': stok,
+      'desc': desc,
+      'kategori': kategori,
+      'pict': pict,
     }).then((value) {
-      fetchObat();  // Refresh the list after updating
+      fetchObat();
     });
+  }
+
+  void _showDeleteConfirmationDialog(BuildContext context, String id) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Hapus Obat'),
+          content: Text('Apakah Anda yakin ingin menghapus obat ini?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                deleteObat(id);
+                Navigator.pop(context);
+              },
+              child: Text('Delete', style: TextStyle(color: Colors.red)),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -119,7 +159,7 @@ class _ManageObatState extends State<ManageObat> {
                         IconButton(
                           icon: Icon(Icons.delete, color: Colors.red),
                           onPressed: () {
-                            deleteObat(item['id']);
+                            _showDeleteConfirmationDialog(context, item['id']);
                           },
                         ),
                       ],
@@ -136,123 +176,294 @@ class _ManageObatState extends State<ManageObat> {
     return format.format(harga);
   }
 
-  // Show dialog to add new obat
   void _showAddObatDialog(BuildContext context) {
     final _namaController = TextEditingController();
     final _hargaController = TextEditingController();
     final _stokController = TextEditingController();
+    final _deskripsiController = TextEditingController();
+    final _fotoController = TextEditingController();
+    String? _selectedKategori; // Variable to store selected category
+
+    // Variables to track validation states
+    bool _isNamaValid = true;
+    bool _isHargaValid = true;
+    bool _isStokValid = true;
+    bool _isDeskripsiValid = true;
+    bool _isFotoValid = true;
+    bool _isKategoriValid = true;
 
     showDialog(
       context: context,
       builder: (context) {
-        return AlertDialog(
-          title: Text('Add Obat'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _namaController,
-                decoration: InputDecoration(labelText: 'Nama Obat'),
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Add Obat'),
+              content: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: _namaController,
+                      decoration: InputDecoration(
+                        labelText: 'Nama Obat',
+                        errorText: !_isNamaValid ? 'Required' : null,
+                      ),
+                    ),
+                    TextField(
+                      controller: _hargaController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Harga Obat',
+                        errorText:
+                            !_isHargaValid ? 'Must be a positive number' : null,
+                      ),
+                    ),
+                    TextField(
+                      controller: _deskripsiController,
+                      decoration: InputDecoration(
+                        labelText: 'Deskripsi Obat',
+                        errorText: !_isDeskripsiValid ? 'Required' : null,
+                      ),
+                    ),
+                    TextField(
+                      controller: _stokController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Stok Obat',
+                        errorText:
+                            !_isStokValid ? 'Must be a positive number' : null,
+                      ),
+                    ),
+                    TextField(
+                      controller: _fotoController,
+                      decoration: InputDecoration(
+                        labelText: 'Foto Obat',
+                        hintText: 'fotoObat/...',
+                        errorText: !_isFotoValid ? 'Required' : null,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    DropdownButtonFormField<String>(
+                      value: _selectedKategori,
+                      items: ['body', 'obat'].map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          _selectedKategori = newValue;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Kategori Obat',
+                        errorText: !_isKategoriValid ? 'Required' : null,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              TextField(
-                controller: _hargaController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(labelText: 'Harga Obat'),
-              ),
-              TextField(
-                controller: _stokController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(labelText: 'Stok Obat'),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                String nama = _namaController.text;
-                int harga = int.tryParse(_hargaController.text) ?? 0;
-                int stok = int.tryParse(_stokController.text) ?? 0;
-                
-                if (nama.isNotEmpty && harga > 0 && stok > 0) {
-                  addObat(nama, harga, stok);
-                  Navigator.pop(context);
-                } else {
-                  // Show error if fields are invalid
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Please fill all fields correctly')),
-                  );
-                }
-              },
-              child: Text('Add'),
-            ),
-          ],
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Validate fields
+                    setState(() {
+                      _isNamaValid = _namaController.text.isNotEmpty;
+                      _isHargaValid =
+                          int.tryParse(_hargaController.text) != null &&
+                              int.parse(_hargaController.text) > 0;
+                      _isStokValid =
+                          int.tryParse(_stokController.text) != null &&
+                              int.parse(_stokController.text) > 0;
+                      _isDeskripsiValid = _deskripsiController.text.isNotEmpty;
+                      _isFotoValid = _fotoController.text.isNotEmpty;
+                      _isKategoriValid = _selectedKategori != null;
+                    });
+
+                    if (_isNamaValid &&
+                        _isHargaValid &&
+                        _isStokValid &&
+                        _isDeskripsiValid &&
+                        _isFotoValid &&
+                        _isKategoriValid) {
+                      addObat(
+                        _namaController.text,
+                        int.parse(_hargaController.text),
+                        int.parse(_stokController.text),
+                        _deskripsiController.text,
+                        _selectedKategori!,
+                        _fotoController.text,
+                      );
+                      Navigator.pop(context);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Please fill all fields correctly'),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('Add'),
+                ),
+              ],
+            );
+          },
         );
       },
     );
   }
 
-  // Show dialog to update obat
   void _showUpdateObatDialog(BuildContext context, Map<String, dynamic> item) {
     final _namaController = TextEditingController(text: item['nama']);
-    final _hargaController = TextEditingController(text: item['harga'].toString());
-    final _stokController = TextEditingController(text: item['stok'].toString());
+    final _hargaController =
+        TextEditingController(text: item['harga'].toString());
+    final _stokController =
+        TextEditingController(text: item['stok'].toString());
+    final _deskripsiController =
+        TextEditingController(text: item['deskripsi'] ?? '');
+    final _fotoController = TextEditingController(text: item['foto'] ?? '');
+    String? _selectedKategori = item['kategori'];
+
+    // Variables for validation
+    bool _isNamaValid = true;
+    bool _isHargaValid = true;
+    bool _isStokValid = true;
+    bool _isDeskripsiValid = true;
+    bool _isFotoValid = true;
+    bool _isKategoriValid = true;
 
     showDialog(
       context: context,
       builder: (context) {
-        return AlertDialog(
-          title: Text('Update Obat'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: _namaController,
-                decoration: InputDecoration(labelText: 'Nama Obat'),
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Update Obat'),
+              content: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      controller: _namaController,
+                      decoration: InputDecoration(
+                        labelText: 'Nama Obat',
+                        errorText: !_isNamaValid ? 'Required' : null,
+                      ),
+                    ),
+                    TextField(
+                      controller: _hargaController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Harga Obat',
+                        errorText:
+                            !_isHargaValid ? 'Must be a positive number' : null,
+                      ),
+                    ),
+                    TextField(
+                      controller: _stokController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Stok Obat',
+                        errorText:
+                            !_isStokValid ? 'Must be a positive number' : null,
+                      ),
+                    ),
+                    TextField(
+                      controller: _deskripsiController,
+                      decoration: InputDecoration(
+                        labelText: 'Deskripsi Obat',
+                        errorText: !_isDeskripsiValid ? 'Required' : null,
+                      ),
+                    ),
+                    TextField(
+                      controller: _fotoController,
+                      decoration: InputDecoration(
+                        labelText: 'Foto Obat',
+                        hintText: 'fotoObat/...',
+                        errorText: !_isFotoValid ? 'Required' : null,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    DropdownButtonFormField<String>(
+                      value: _selectedKategori,
+                      items: ['body', 'obat'].map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          _selectedKategori = newValue;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Kategori Obat',
+                        errorText: !_isKategoriValid ? 'Required' : null,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              TextField(
-                controller: _hargaController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(labelText: 'Harga Obat'),
-              ),
-              TextField(
-                controller: _stokController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(labelText: 'Stok Obat'),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                String nama = _namaController.text;
-                int harga = int.tryParse(_hargaController.text) ?? 0;
-                int stok = int.tryParse(_stokController.text) ?? 0;
-                
-                if (nama.isNotEmpty && harga > 0 && stok > 0) {
-                  updateObat(item['id'], nama, harga, stok);
-                  Navigator.pop(context);
-                } else {
-                  // Show error if fields are invalid
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Please fill all fields correctly')),
-                  );
-                }
-              },
-              child: Text('Update'),
-            ),
-          ],
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _isNamaValid = _namaController.text.isNotEmpty;
+                      _isHargaValid =
+                          int.tryParse(_hargaController.text) != null &&
+                              int.parse(_hargaController.text) > 0;
+                      _isStokValid =
+                          int.tryParse(_stokController.text) != null &&
+                              int.parse(_stokController.text) > 0;
+                      _isDeskripsiValid = _deskripsiController.text.isNotEmpty;
+                      _isFotoValid = _fotoController.text.isNotEmpty;
+                      _isKategoriValid = _selectedKategori != null;
+                    });
+
+                    if (_isNamaValid &&
+                        _isHargaValid &&
+                        _isStokValid &&
+                        _isDeskripsiValid &&
+                        _isFotoValid &&
+                        _isKategoriValid) {
+                      updateObat(
+                        item['id'],
+                        _namaController.text,
+                        int.parse(_hargaController.text),
+                        int.parse(_stokController.text),
+                        _deskripsiController.text,
+                        _selectedKategori!,
+                        _fotoController.text,
+                      );
+                      Navigator.pop(context);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Please fill all fields correctly'),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('Update'),
+                ),
+              ],
+            );
+          },
         );
       },
     );
